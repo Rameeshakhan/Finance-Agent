5abe0341739680274dd0a5659f7079878bb3621f
Rameesha Khan <rameeshakhan75@gmail.com>
2026-05-31 23:09:04 +0500

    server name changed to local host

--- app.py
+++ app.py
@@ -206,7 +206,7 @@ with gr.Blocks() as demo:
 if __name__ == "__main__":
     demo.launch(
         share=False,
-        server_name="0.0.0.0",
+        server_name="localhost",
         server_port=7860,
         theme=gr.themes.Soft(primary_hue="blue"),
         css=CSS,
